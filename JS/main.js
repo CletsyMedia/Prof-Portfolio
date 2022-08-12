6e7b4480d527ba514576028482b7d7a94b103e25
CletsyMedia <109830247+CletsyMedia@users.noreply.github.com>
2022-08-12 03:30:41 +0100

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,110 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is > 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header');
+    else header.classList.remove('scrool-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/*=============== SERVICES MODAL ===============*/
+const modalView = document.querySelectorAll('.services_modal'),
+      modalBtns = document.querySelectorAll('.services_button'),
+      modalClose = document.querySelectorAll('.services_modal-close');
+
+let modal = function(modalClick){
+    modalView[modalClick].classList.add('active-modal')
+}
+modalBtns.forEach((mb, i) =>{
+    mb.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) =>{
+    mc.addEventListener('click', () => {
+        modalView.forEach((mv) => {
+            mv.classList.remove('active-modal')
+        })
+    })
+})
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work_container', {
+    selectors:{
+        target: '.work_card'
+    }, animation:{
+        duration: 300
+    }
+});
+
+/* Link active work */ 
+const changeWork = document.querySelectorAll('.work_item')
+
+function activeWork(){
+    changeWork.forEach(c=> i.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+// changeWork.forEach(c => i.addEventListener('click', 'activeWork'))
+
+/*===============Slider  TESTIMONIAL(No SWIPER Due to regular class update)===============*/
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" testy", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " testy";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop  = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+              if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+              }else{
+                document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+              }
+    })
+}
+window.addEventListener('scroll', scrollActive);
+
+/*=============== LIGHT DARK THEME ===============*/ 
+let theme = document.querySelector('#theme-button');
+  theme.onclick = () =>{
+    theme.classList.toggle('fa-sun');
+
+    if(theme.classList.contains('fa-sun')){
+      document.body.classList.add('light-theme');
+    }else{
+      document.body.classList.remove('light-theme');
+    }
+  };
+
+/*=============== AOS SCROLL REVEAL ANIMATION ===============*/
+AOS.init({
+    offset: 400,
+    duration: 1000,
+    easing: 'ease-in-out'
+  });
