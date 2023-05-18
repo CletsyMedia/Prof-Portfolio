1332688f093a4b37d1c88dd9729d3a1d81e27d60
CletsyMedia <Cletussam12@yahoo.com>
2023-05-18 04:00:52 +0100

    Update

--- JS/main.js
+++ JS/main.js
@@ -153,9 +153,7 @@ function onError(contactName, message){
 function onSuccess(contactName){
   let parent = contactName.parentElement;
   let msgEle = parent.querySelector("span");
-  // let validEl = parent.querySelector("small");
   msgEle.style.display = "none";
-  // validEl.style.display="block";
   parent.classList.remove("onError");
   contactBox.style.border="1px solid hsl(154, 59%, 51%)";
   parent.classList.add("onSuccess");
